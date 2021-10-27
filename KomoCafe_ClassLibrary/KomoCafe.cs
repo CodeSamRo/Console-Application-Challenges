e6009fbce48d7cfd3e928e657cc2015275854387
Samuel <codesamro@gmail.com>
2021-10-26 21:46:20 -0400

    KomoCafe Poco layout finished

--- KomoCafe_ClassLibrary/KomoCafe.cs
+++ KomoCafe_ClassLibrary/KomoCafe.cs
@@ -8,6 +8,19 @@ namespace KomoCafe_ClassLibrary
 {
     public class KomoCafe
     {
+        //A meal number, meal name, description, list of ingredients, price
+        public int MealNumber { get; }
+        public string MealName { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public int Price { get; set; }
+        public KomoCafe()
+        {
 
+        }
+        public KomoCafe(int mealNumber, string mealName, string description, string ingredidents, int price)
+        {
+
+        }
     }
 }
