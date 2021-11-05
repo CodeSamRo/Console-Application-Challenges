0de51df5da6aa961834711f95aa46d222bb49f0d
Samuel <codesamro@gmail.com>
2021-11-04 21:11:09 -0400

    Updated functions

--- KomoBadges_ClassLibrary/KomoBadgesREPO.cs
+++ KomoBadges_ClassLibrary/KomoBadgesREPO.cs
@@ -12,6 +12,16 @@ namespace KomoBadges_ClassLibrary
         //key for the dictionary will be the badgeID
         //the value for the dictionary will be the list of door names
         Dictionary<int, List<string>> newBadge = new Dictionary<int, List<string>>();
+        List<KomoBadges> KomoBadges = new List<KomoBadges>();
+        public void CreateID(KomoBadges komoBadges)
+        {
+            KomoBadges.Add(komoBadges);
+        }
+        public void DeleteID(KomoBadges komoBadges)
+        {
+            KomoBadges.Remove(komoBadges);
+        }
+
         public void CreateBadge(KomoBadges komoBadges)
         {
             newBadge.Add(komoBadges.BadgeID, new List<string>());
@@ -20,9 +30,24 @@ namespace KomoBadges_ClassLibrary
         {
             newBadge[badgeID.BadgeID].Add(door);
         }
+        public void RemoveDoor(KomoBadges badgeID, string door)
+        {
+            newBadge[badgeID.BadgeID].Remove(door);
+        }
         public Dictionary<int, List<string>> ViewBadge()
         {
             return newBadge;
+        }
+        public KomoBadges GetBadgeByID(int ID)
+        {
+            foreach (KomoBadges badges in KomoBadges)
+            {
+                if (ID == badges.BadgeID)
+                {
+                    return badges;
+                }
+            }
+            return null;
         }
         public void RemoveBadge(KomoBadges komoBadges)
         {
--- KomoBadges_ConsoleApp/ProgramUI.cs
+++ KomoBadges_ConsoleApp/ProgramUI.cs
@@ -58,8 +58,6 @@ namespace KomoBadges_ConsoleApp
         }
         private void CreateBadge()
         {
-            Console.Clear();
-            Console.WriteLine("What would you like the badge ID to be...\n");
             BadgeID();
             Console.WriteLine("\nPlease anything to continue...");
             Console.ReadKey();
@@ -72,90 +70,180 @@ namespace KomoBadges_ConsoleApp
             bool run = true;
             while (run)
             {
+                Console.Clear();
+                Console.WriteLine("What would you like the badge ID to be...\n");
                 try
                 {
+                    bool continueToRun = true;
                     int userID = Convert.ToInt32(Console.ReadLine());
                     foreach (var badge in listOfBadges)
                     {
-                        if (komoBadges.BadgeID == userID)
+                        if (listOfBadges.Keys.Contains(userID))
                         {
                             Console.Clear();
                             Console.WriteLine("BadgeID already Exists! Please try again. Press anything to continue...");
                             Console.ReadKey();
-                            run = false;
-                        }
-                        else 
-                        {
-                            komoBadges.BadgeID = userID;
+                            continueToRun = false;
                         }
                     }
-                    komoBadges.BadgeID = userID;
-                    bool secondRun = true;
-                    Console.WriteLine("What door should {0} have access to?", komoBadges.BadgeID);
-                    string doorName = Console.ReadLine();               
-                    komoBadgesREPO.CreateBadge(komoBadges);
-                    komoBadgesREPO.AddDoor(komoBadges, doorName);
-                    Console.WriteLine("Badge number {0} was created.", komoBadges.BadgeID);
-                    while (secondRun)
+                    if (continueToRun)
                     {
-                        Console.WriteLine("\nAny other doors? (Y/N)");
-                        string userInput = Console.ReadLine().ToLower();
-                        switch (userInput)
+                        komoBadges.BadgeID = userID;
+                        bool secondRun = true;
+                        Console.WriteLine("What door should {0} have access to?", komoBadges.BadgeID);
+                        string doorName = Console.ReadLine().ToUpper();
+                        komoBadges.DoorNames = null;
+                        komoBadgesREPO.CreateID(komoBadges);
+                        komoBadgesREPO.CreateBadge(komoBadges);
+                        komoBadgesREPO.AddDoor(komoBadges, doorName);
+                        Console.WriteLine("Badge number {0} was created.", komoBadges.BadgeID);
+                        while (secondRun)
                         {
-                            case "y":
-                                Console.WriteLine("\nWhat door should {0} have access to?\n", komoBadges.BadgeID);
-                                string doorInput = Console.ReadLine();
-                                komoBadgesREPO.AddDoor(komoBadges, doorInput);
-                                break;
-                            case "n":
-                                secondRun = false;
-                                break;
-                            default:
-                                Console.WriteLine("\nPlease type Y for Yes or N for No...\n");
-                                Console.ReadKey();
-                                break;
-                        }
+                            Console.WriteLine("\nAny other doors? (Y/N)");
+                            string userInput = Console.ReadLine().ToLower();
+                            switch (userInput)
+                            {
+                                case "y":
+                                    Console.WriteLine("\nWhat door should {0} have access to?\n", komoBadges.BadgeID);
+                                    string doorInput = Console.ReadLine().ToUpper();
+                                    komoBadgesREPO.AddDoor(komoBadges, doorInput);
+                                    break;
+                                case "n":
+                                    secondRun = false;
+                                    break;
+                                default:
+                                    Console.WriteLine("\nPlease type Y for Yes or N for No...\n");
+                                    Console.ReadKey();
+                                    break;
+                               }
 
+                        }
                     }
                     run = false;
                 }
                 catch
                 {
-                    Console.Clear();
-                    DefaultErrorMessage();
-                    Console.ReadKey();
+                    DefaultErrorMessage();                   
                 }
             }
         }
         private void EditBadge()
         {
+            ListOfBadges();
+            Console.WriteLine("Type in the Badge ID you would like to edit.");
+            try
+            {
+                Dictionary<int, List<string>> listOfBadges = komoBadgesREPO.ViewBadge();
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                KomoBadges komoBadges = komoBadgesREPO.GetBadgeByID(userInput);
+
+                foreach (var badgeID in listOfBadges)
+                {
+                    if (userInput == badgeID.Key)
+                    {
+                        EditPropmt(komoBadges, listOfBadges);
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("ID does not exist.");
+                        Console.ReadKey();
+                    }
+                }
+            }
+            catch
+            {
+                DefaultErrorMessage();
+            }
 
         }
+        private void EditPropmt(KomoBadges komoBadges, Dictionary<int, List<string>> listOfBadges)
+        {
+            bool run = true;
+            
+            while (run)
+            {
+                Console.WriteLine("What would you like to do?\n" +
+                "1. Remove a Door\n" +
+                "2. Add a door\n" +
+                "3. Exit");
+                string userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("What door should we remove from {0}\n", komoBadges.BadgeID);
+                        string removeDoor = Console.ReadLine().ToUpper();
+                        foreach (var badge in listOfBadges)
+                        {
+                            if (badge.Value.Contains(removeDoor))
+                            {
+                                komoBadgesREPO.RemoveDoor(komoBadges, removeDoor);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Door is not listed under {0}", komoBadges.BadgeID);
+                            }
+                        }
+                        Console.WriteLine("Door has been removed. Press anything to continue...");
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("\nWhat door should {0} have access to?\n", komoBadges.BadgeID);
+                        string addDoor = Console.ReadLine();
+                        foreach (var badge in listOfBadges)
+                        {
+                            if (badge.Value.Contains(addDoor))
+                            {
+                                Console.WriteLine("Door Already Exits.");
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                komoBadgesREPO.AddDoor(komoBadges, addDoor);
+                            }
+                        }
+                        Console.WriteLine("Door has been added. Press anything to continue...");
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        run = false;
+                        break;
+                    default:
+                        DefaultErrorMessage();
+                        break;
+                }
+            }
+        }
+
         private void ListOfBadges()
         {
             Console.Clear();
             Dictionary<int, List<string>> listOfBadges = komoBadgesREPO.ViewBadge();
+            List<string> valueColl;
             Console.WriteLine("Badge#          Door Access");
-            Dictionary<int, List<string>>.ValueCollection valueColl;
             foreach (KeyValuePair<int, List<string>> badge in listOfBadges)
             {
-                valueColl = listOfBadges.Values;
+                valueColl = badge.Value;
                 Console.WriteLine($"{badge.Key}            {ListofDoors(valueColl)}");
             }
             Console.ReadKey();
         }
-        private string ListofDoors(Dictionary<int, List<string>>.ValueCollection valueColl)
+        private string ListofDoors(List<string> valueColl)
         {
             string doors = null;
-            foreach(List<string> door in valueColl)
+            foreach(string door in valueColl)
             {
-                doors = string.Join(",", door);
+                doors = string.Join(", ", door);
             }
             return doors;
         }
         private void DefaultErrorMessage()
         {
+            Console.Clear();
             Console.WriteLine("Please type in the correct input...");
+            Console.ReadKey();
         }
     }
 }
