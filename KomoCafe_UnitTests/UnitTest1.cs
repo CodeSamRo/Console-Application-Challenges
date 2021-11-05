b92a7ca89443fa7e98458d84c973bc143f834e21
Samuel <codesamro@gmail.com>
2021-11-05 12:32:35 -0400

    Unit Test

--- KomoCafe_UnitTests/UnitTest1.cs
+++ KomoCafe_UnitTests/UnitTest1.cs
@@ -9,11 +9,14 @@ namespace KomoCafe_UnitTests
     {
         private KomoCafeRepo komoCafeRepo;
         private KomoCafe komoCafe;
+        private KomoCafe komoCafe2;
         [TestInitialize]
         public void Arrange()
         {
             komoCafeRepo = new KomoCafeRepo();
             komoCafe = new KomoCafe(1, "Red Soup", "It is red and it is soup", "Water, RedDye", 10.50);
+            komoCafe2 = new KomoCafe(2, "Soup", "It is soup", "Hot Water", 5.00);
+            komoCafeRepo.CreateMeal(komoCafe2);
         }
         [TestMethod]
         public void CreateMeal_Test()
@@ -24,8 +27,8 @@ namespace KomoCafe_UnitTests
         [TestMethod]
         public void DeleteMeal_Test()
         {
-            bool wasDeleted = komoCafeRepo.DeleteMeal(komoCafe);
-            Assert.IsFalse(wasDeleted);
+            bool wasDeleted = komoCafeRepo.DeleteMeal(komoCafe2);
+            Assert.IsTrue(wasDeleted);
         }
         [TestMethod]
         public void ViewMeal_Test()
