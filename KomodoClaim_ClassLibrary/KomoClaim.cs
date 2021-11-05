ae117e391d33bdea12f60af8dbd063140dfe5dc9
Samuel <codesamro@gmail.com>
2021-11-05 12:15:44 -0400

    Basic Layout

--- KomodoClaim_ClassLibrary/KomoClaim.cs
+++ KomodoClaim_ClassLibrary/KomoClaim.cs
@@ -9,7 +9,7 @@ namespace KomodoClaim_ClassLibrary
     public class KomoClaim
     {
         public int ClaimID { get; set; }
-        public string ClaimType { get; set; }
+        public string[] ClaimType  { get; set;  }
         public string Description { get; set; }
         public int ClaimAmount { get; set; }
         public DateTime DateOfIncident { get; set; }
@@ -19,7 +19,7 @@ namespace KomodoClaim_ClassLibrary
         {
 
         }
-        public KomoClaim(int claimID, string claimType, string description, int claimAmount, DateTime dateOfIncident, DateTime dateOfClaim, bool isValid)
+        public KomoClaim(int claimID, string[] claimType, string description, int claimAmount, DateTime dateOfIncident, DateTime dateOfClaim, bool isValid)
         {
             ClaimID = claimID;
             ClaimType = claimType;
