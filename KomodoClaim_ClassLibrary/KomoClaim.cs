2a61e788d6aa3c4243072753b955c86800618211
Samuel <codesamro@gmail.com>
2021-11-04 21:35:14 -0400

    Basic Layout

---
+++ KomodoClaim_ClassLibrary/KomoClaim.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomodoClaim_ClassLibrary
+{
+    public class KomoClaim
+    {
+        public int ClaimID { get; set; }
+        public string ClaimType { get; set; }
+        public string Description { get; set; }
+        public int ClaimAmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        public bool IsValid { get; set; }
+        public KomoClaim()
+        {
+
+        }
+        public KomoClaim(int claimID, string claimType, string description, int claimAmount, DateTime dateOfIncident, DateTime dateOfClaim, bool isValid)
+        {
+            ClaimID = claimID;
+            ClaimType = claimType;
+            Description = description;
+            ClaimAmount = claimAmount;
+            DateOfIncident = dateOfIncident;
+            DateOfClaim = dateOfClaim;
+            IsValid = isValid;
+        }
+    }
+}
---
+++ KomodoClaim_UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KomodoClaim_UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KomodoClaim_UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5c6904a4-f58d-486d-a22b-73ab2319dca3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
